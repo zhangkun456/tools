6f0e4f97bf1458f4e2d7b1efecd91c5b4948ad56
凭海 <notepaper.zk@alibaba-inc.com>
2017-10-01 15:07:05 +0800

    添加release版跳转支持

--- YoukuEntryDemo/app/src/main/java/demo/entry/zk/com/youkuentrydemo/MainActivity.java
+++ YoukuEntryDemo/app/src/main/java/demo/entry/zk/com/youkuentrydemo/MainActivity.java
@@ -13,16 +13,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 import com.uuzuche.lib_zxing.activity.CaptureActivity;
 import com.uuzuche.lib_zxing.activity.CodeUtils;
 
-import java.lang.reflect.Method;
-import java.net.URI;
-import java.net.URL;
 import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -138,10 +135,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    private static String toHexString(String s)
+    {
+        String str="";
+        for (int i=0;i<s.length();i++)
+        {
+            int ch = (int)s.charAt(i);
+            String s4 = Integer.toHexString(ch);
+            str = str + s4;
+        }
+        return str;
+    }
+
     private void openNativePage(String url) {
-        Intent intent = new Intent();
-        intent.setData(Uri.parse("youku://planet/planet_test?url=" + url));
-        startActivity(intent);
+        try {
+            Intent intent = new Intent();
+
+            String param = "https://weex.alibabaplanet.com/wow/planet/act/entry-demo?wh_weex=true"
+                    + "&navigatorurl=" + toHexString(URLEncoder.encode(url));
+
+            param = URLEncoder.encode(param);
+            System.out.println("openNativePage =" + param);
+            intent.setData(Uri.parse("youku://planet/weex?_wx_tpl=" + param));
+            startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void openWeex(String url) {
